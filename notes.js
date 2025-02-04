68cb6ecc76084607f98db2f6685f2d563fa142e8
AvaniSujith <126156788+AvaniSujith@users.noreply.github.com>
2025-02-04 22:05:04 +0530

    new corrections

--- notes.js
+++ notes.js
@@ -222,6 +222,115 @@
 // lesson - 13 Simple API 
 
 
+// const fs = require('fs');
+// const http = require('http');
+// const { hasUncaughtExceptionCaptureCallback } = require('process');
+// const url = require('url');
+
+// const server = http.createServer((req, res) =>{
+//     const pathName = req.url;
+
+//     if(pathName === '/' || pathName === '/overview'){
+
+//         res.end('This is the OVERVIEW');
+
+//     }else if(pathName === '/product'){
+
+//         res.end('This is the PRODUCT');
+
+//     }else if(pathName === '/api'){
+
+//         fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) =>{
+//             const productDetails = JSON.parse(data);
+//             // console.log(productDetails);
+
+//         });
+
+//         res.end('API');
+//     }else{
+    
+//         res.writeHead(404, {
+//             'Content-type' : 'text/html',
+//             'my-own-header': 'hello-world'
+//         });
+
+//         res.end('<h1>Page not foundd</h1>');
+//     }
+// });
+
+// server.listen(8000, '127.0.0.1', ()=>{
+//     console.log('Listening to request at port 800');
+// });
+
+
+// lesson - 14 
+
+// all templates built 
+
+// lesson - 15 
+
+// const fs = require('fs');
+// const http = require('http');
+// const url = require('url');
+// const slugify = require('slugify');
+// const replaceTemplate = require('./modules/replaceTemplate');
+
+
+// const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
+// const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+// const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
+  
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// const dataObj = JSON.parse(data);
+  
+// const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+// console.log(slugs);
+  
+// const server = http.createServer((req, res) => {
+// const { query, pathname } = url.parse(req.url, true);
+  
+//     // Overview page
+// if (pathname === '/' || pathname === '/overview') {
+//     res.writeHead(200, {
+//     'Content-type': 'text/html'
+//     });
+
+//     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+//     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+//     res.end(output);
+
+//     // Product page
+// } else if (pathname === '/product') {
+//     res.writeHead(200, {
+//     'Content-type': 'text/html'
+//     });
+//     const product = dataObj[query.id];
+//     const output = replaceTemplate(tempProduct, product);
+//     res.end(output);
+
+//     // API
+// } else if (pathname === '/api') {
+//     res.writeHead(200, {
+//     'Content-type': 'application/json'
+//     });
+//     res.end(data);
+
+//     // Not found
+// } else {
+//     res.writeHead(404, {
+//     'Content-type': 'text/html',
+//     'my-own-header': 'hello-world'
+//     });
+//     res.end('<h1>Page not found!</h1>');
+// }
+// });
+
+// server.listen(8000, '127.0.0.1', () => {
+// console.log('Listening to requests on port 8000');
+// });
+ 
+
+
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -230,6 +339,7 @@ const server = http.createServer((req, res) =>{
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview'){
+
 
         res.end('This is the OVERVIEW');
 
@@ -261,6 +371,4 @@ server.listen(8000, '127.0.0.1', ()=>{
     console.log('Listening to request at port 800');
 });
 
-
-// lesson - 14 
 
